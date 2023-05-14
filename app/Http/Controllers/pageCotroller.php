79e703c409ecbb105578bbd871346edf98b4d6de
alicecroce <alicecroce2@gmail.com>
2023-05-14 16:01:19 +0200

    rparato

--- app/Http/Controllers/pageCotroller.php
+++ app/Http/Controllers/pageCotroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class pageController extends Controller
+class pageCotroller extends Controller
 {
 
     public static $flights = [
